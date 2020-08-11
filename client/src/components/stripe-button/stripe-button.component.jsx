dcd7a7c1cef5eedbada10dfc7bf46a9e8a8a8752
Giuliano Gabella <ggabella91@gmail.com>
2020-08-10 19:20:19 -0700

    Separated CRA app into client folder, added express server to help handle backend stripe payments.

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = `pk_test_51H7ZthDSfpyWawQhjKKZSllBUqjqyMXoijHtJmcCZPjy7NW26pIHEOw5hVqCRwRJGoBGk8nYEkztF0ADBgPeoZmr00E8RezAUK`;
+
+  const onToken = (token) => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Pleas make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
